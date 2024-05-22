7368c725b79e990a8fd3c10d87c6d62d599c637e
chesedo <pieter@chesedo.me>
2024-05-22 21:28:32 +0100

    Basic design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.{html,js}"],
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'off-white': '#f5f5f5',
+        'gold-premium': '#DAA520',
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
