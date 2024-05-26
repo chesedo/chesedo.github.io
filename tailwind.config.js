b602a6e9bc750a38a83892e23f96d99ad794e9d6
chesedo <pieter@chesedo.me>
2024-05-26 17:39:16 +0100

    Improve lighthouse score
    Reduces fonts and images. Also some tailwind optimizations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.{html,js}"],
-  darkMode: false,
   theme: {
     extend: {
-      colors: {
-        'off-white': '#f5f5f5',
-        'gold-premium': '#DAA520',
-      },
       fontFamily: {
         'raleway': ['Raleway', 'sans-serif'],
         'lato': ['Lato', 'sans-serif'],
