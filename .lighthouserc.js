e8feea96df391f8a9337517bec0a75eeea11cc3f
chesedo <pieter@chesedo.me>
2024-08-16 16:28:41 +0200

    Lower accessibility threshold for blog posts

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -28,7 +28,7 @@ module.exports = {
           preset: 'lighthouse:no-pwa',
           assertions: {
             'categories:performance': ['error', {minScore: 0.95}],
-            'categories:accessibility': ['error', {minScore: 0.95}],
+            'categories:accessibility': ['error', {minScore: 0.93}],
             'categories:best-practices': ['error', {minScore: 0.95}],
             'categories:seo': ['error', {minScore: 0.95}],
 
