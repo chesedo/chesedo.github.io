81d955dc6c5200c06654627e06cef6a92079f032
chesedo <pieter@chesedo.me>
2025-03-10 10:07:13 +0200

    Update dev environment

--- shell.nix
+++ shell.nix
@@ -3,14 +3,14 @@ let
     "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
   # Pin to stable from https://status.nixos.org/
   nixpkgs = import (fetchTarball
-    "https://github.com/NixOS/nixpkgs/archive/752c634c09ceb50c45e751f8791cb45cb3d46c9e.tar.gz") {
+    "https://github.com/NixOS/nixpkgs/archive/52e3095f6d812b91b22fb7ad0bfc1ab416453634.tar.gz") {
       overlays = [ moz_overlay ];
     };
 in with nixpkgs;
 stdenv.mkDerivation {
   name = "moz_overlay_shell";
   buildInputs = with nixpkgs; [
-    ((rustChannelOf { channel = "1.80.0"; }).rust.override {
+    ((rustChannelOf { channel = "1.85.0"; }).rust.override {
       extensions = [ "rust-src" ];
     })
     cargo-watch
